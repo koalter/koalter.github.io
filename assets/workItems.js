020dfe72a4f70ec3d28bf1dc75cba7a7d897c89c
koalter <heavens_drive090@hotmail.com>
2021-08-18 00:23:45 -0300

    redesign

---
+++ assets/workItems.js
@@ -0,0 +1,17 @@
+let workItems = [
+  {
+    title: "Front-end",
+    description: "• Web design \n• Knowledge in Sass and Bootstrap \n• Experience with Angular and React",
+    icon: "fab fa-angular"
+  },
+  {
+    title: "Back-end",
+    description: "• Design and development of APIs \n• Experience with .NET Framework, .NET Core and NodeJS \n• Management of business logic",
+    icon: "fab fa-python"
+  },
+  {
+    title: "Databases",
+    description: "• Experience with MSSQL and MySQL \n• Experience with Non-relational databases (MongoDB)",
+    icon: "fas fa-database"
+  }
+];
